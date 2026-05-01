1228dc78171669faa8de2a94561301c1c9ebe29a
mgadek84 <86467770+mgadek84@users.noreply.github.com>
2026-05-01 12:30:26 +0200

    Clean up and config modification, leaving only chromium as test browser, changing default test folder in config

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -4,12 +4,12 @@ import { test, expect } from '@playwright/test';
 
 test("Verify page title",async ({page})=>{
 
-    await page.goto("vwb5fl.pl");
+    //await page.goto("www.vwb5fl.pl");
+    await page.goto("http://vwb5fl.pl");
 
     let title:string=await page.title();
         console.log("Title:",title);
 
-    await expect(page).toHaveTitle("VWB5FL");
+    await expect(page).toHaveTitle("This is the title of the webpage!");
 
 })
-
